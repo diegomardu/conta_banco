a85d9588c81cac4b7b9bfaf550d3bb23de7b9d7c
Diego Martins Duarte <diegomartins836@gmail.com>
2019-05-31 17:15:46 -0300

    Classe conta corrente criada

---
+++ conta_corrente.py
@@ -0,0 +1,10 @@
+from conta import *
+class conta_corrente(conta):
+    def __init__(self,numero,saldo,limite):
+        super().__init__(numero,saldo)
+        self._limite = limite
+
+    def getLimite(self):
+        return self._limite
+    def setLimite(self,limite):
+        self._limite = limite
